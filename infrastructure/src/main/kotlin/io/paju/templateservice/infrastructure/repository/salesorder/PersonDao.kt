18ce50ef026e41a9e4d26bb7a06755bc3579787c
KimmoEklund <kimmo.eklund@nemit.fi>
2017-11-01 15:20:42 +0200

    SQL strings as multiline Strings

--- infrastructure/src/main/kotlin/io/paju/templateservice/infrastructure/repository/salesorder/PersonDao.kt
+++ infrastructure/src/main/kotlin/io/paju/templateservice/infrastructure/repository/salesorder/PersonDao.kt
@@ -10,15 +10,18 @@ import org.jdbi.v3.core.generic.GenericType
 class PersonDao(val jdbi: Jdbi) {
     fun insert(data: Person): Person {
         return jdbi.withHandle<Person, Exception> { handle ->
-            val sql = "INSERT INTO person (first_name, last_name, sex, date_of_birth) " +
-                    "VALUES (:first_name, :last_name, :sex, :date_of_birth)"
+            val sql =
+                """
+                    INSERT INTO person (first_name, last_name, sex, date_of_birth)
+                    VALUES (:first_name, :last_name, :sex, :date_of_birth)
+                """
             val map = handle.createUpdate(sql)
-                    .bind("first_name", data.firstName)
-                    .bind("last_name", data.lastName)
-                    .bind("sex", data.sex)
-                    .bind("date_of_birth", data.dateOfBirth)
-                    .executeAndReturnGeneratedKeys()
-                    .collectInto(object : GenericType<Map<Person, Long>>(){})
+                .bind("first_name", data.firstName)
+                .bind("last_name", data.lastName)
+                .bind("sex", data.sex)
+                .bind("date_of_birth", data.dateOfBirth)
+                .executeAndReturnGeneratedKeys()
+                .collectInto(object : GenericType<Map<Person, Long>>() {})
             val person = map.keys.first()
             val id = map.getOrDefault(person, -1) // should return error instead
             person.setValueObjectLocalId(id)
@@ -28,23 +31,28 @@ class PersonDao(val jdbi: Jdbi) {
 
     fun insert(salesOrderId: SalesOrderId, data: ParticipantAndRole) {
         jdbi.useHandle<Exception>({ handle ->
-            val sql = "INSERT INTO person_role_in_sales_order (sales_order_id, person_id, role) " +
-                    "VALUES (:sales_order_id, :person_id, :role)"
+            val sql =
+                """
+                    INSERT INTO person_role_in_sales_order (sales_order_id, person_id, role)
+                    VALUES (:sales_order_id, :person_id, :role)
+                """
 
             val update = handle.createUpdate(sql)
             update.bind("sales_order_id", salesOrderId.value)
-                    .bind("person_id", data.participant.valueObjectLocalId())
-                    .bind("role", data.role)
+                .bind("person_id", data.participant.valueObjectLocalId())
+                .bind("role", data.role)
         })
     }
 
     fun findParticipants(id: SalesOrderId): List<ParticipantAndRole> {
-        val map = jdbi.withHandle<Map<Person, String>, Exception>( { handle ->
-            val sql ="SELECT p.id, p.first_name, p.last_name, p.sex, p.date_of_birth, r.role FROM person p, " +
-                    "person_role_in_sales_order r WHERE r.person_id = p.id and r.sales_order_id = :id"
-
+        val map = jdbi.withHandle<Map<Person, String>, Exception>({ handle ->
+            val sql =
+                """
+                    SELECT p.id, p.first_name, p.last_name, p.sex, p.date_of_birth, r.role FROM person p,
+                        person_role_in_sales_order r WHERE r.person_id = p.id and r.sales_order_id = :id
+                """
             val query = handle.createQuery(sql)
-            val results: Map<Person, String> = query.bind("id", id.value).collectInto(object : GenericType<Map<Person, String>>(){})
+            val results: Map<Person, String> = query.bind("id", id.value).collectInto(object : GenericType<Map<Person, String>>() {})
             results
         })
         val list = mutableListOf<ParticipantAndRole>()
@@ -56,7 +64,7 @@ class PersonDao(val jdbi: Jdbi) {
 
     fun delete(data: Person) {
         jdbi.useHandle<Exception>({ handle ->
-            val sql ="DELETE FROM person WHERE id = :id"
+            val sql = "DELETE FROM person WHERE id = :id"
             val update = handle.createUpdate(sql)
             update.bind("id", data.valueObjectLocalId())
             update.execute()
