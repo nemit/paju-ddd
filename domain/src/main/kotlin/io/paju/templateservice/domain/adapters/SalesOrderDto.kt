397e323b520766ecd993098a7a12ec25e1439ab7
Ville Vuori <ville.vuori@nemit.fi>
2017-11-04 20:35:59 +0200

    Remove current structure

--- domain/src/main/kotlin/io/paju/templateservice/domain/adapters/SalesOrderDto.kt
+++
@@ -1,8 +0,0 @@
-package io.paju.templateservice.domain.adapters
-
-import io.paju.templateservice.domain.salesorder.ParticipantAndRole
-import io.paju.templateservice.domain.salesorder.ProductAndStatus
-import io.paju.templateservice.domain.salesorder.ServiceAndStatus
-import java.util.*
-
-data class SalesOrderDto(val id: UUID, val services: List<ServiceAndStatus>, val products: List<ProductAndStatus>, val participants: List<ParticipantAndRole>)
