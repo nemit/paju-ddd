cb3ca8f5c88e89aa5fc5a1d4b470541af5a6ad47
Ville Vuori <ville.vuori@nemit.fi>
2017-11-05 12:30:52 +0200

    Add sealed classes and more type safety

--- ddd-core/src/main/kotlin/io/paju/ddd/AggregateRootStateExtractor.kt
+++
@@ -1,5 +0,0 @@
-package io.paju.ddd
-
-interface AggregateRootStateExtractor<in A: AggregateRoot, out S: AggregateRootState> {
-    fun extractState(aggregate: A) : S
-}
--- example/ddd-salesorder-domain/src/main/kotlin/io/paju/salesorder/domain/event/Events.kt
+++ example/ddd-salesorder-domain/src/main/kotlin/io/paju/salesorder/domain/event/Events.kt
@@ -8,13 +8,15 @@ import io.paju.salesorder.domain.PaymentMethod
 import io.paju.salesorder.domain.PaymentStatus
 import io.paju.salesorder.domain.Product
 
+sealed class SalesOrderEvent: Event()
+
 // Sales order Events
 data class SalesOrderCreated(
     val id: AggregateRootId,
     val customer: EntityId
 )
-data class SalesOrderDeleted(override val id: AggregateRootId) : Event()
-data class SalesOrderConfirmed(override val id: AggregateRootId) : Event()
+data class SalesOrderDeleted(override val id: AggregateRootId) : SalesOrderEvent()
+data class SalesOrderConfirmed(override val id: AggregateRootId) : SalesOrderEvent()
 
 // Product Events
 data class ProductAdded(
@@ -23,8 +25,8 @@ data class ProductAdded(
     val paymentStatus: PaymentStatus,
     val paymentMethod: PaymentMethod,
     val deliveryStatus: DeliveryStatus
-) : Event()
-data class ProductRemoved(override val id: AggregateRootId, val product: Product) : Event()
-data class ProductDelivered(override val id: AggregateRootId, val product: Product) : Event()
-data class ProductInvoiced(override val id: AggregateRootId, val product: Product) : Event()
-data class ProductPayed(override val id: AggregateRootId, val product: Product) : Event()
+) : SalesOrderEvent()
+data class ProductRemoved(override val id: AggregateRootId, val product: Product) : SalesOrderEvent()
+data class ProductDelivered(override val id: AggregateRootId, val product: Product) : SalesOrderEvent()
+data class ProductInvoiced(override val id: AggregateRootId, val product: Product) : SalesOrderEvent()
+data class ProductPayed(override val id: AggregateRootId, val product: Product) : SalesOrderEvent()
