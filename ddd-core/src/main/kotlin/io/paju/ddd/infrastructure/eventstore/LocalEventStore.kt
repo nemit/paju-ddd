64c3e1076156cbb077a8a7bbc42da7fcdc91d1ee
Ville Vuori <ville.vuori@nemit.fi>
2017-11-06 18:38:24 +0200

    Add thread safety to local event store

--- ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/eventstore/LocalEventStore.kt
+++ ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/eventstore/LocalEventStore.kt
@@ -7,35 +7,44 @@ import io.paju.ddd.infrastructure.EventStoreReader
 import io.paju.ddd.infrastructure.EventStoreWriter
 import org.slf4j.LoggerFactory
 import java.util.ConcurrentModificationException
+import java.util.concurrent.locks.ReentrantLock
+
 
 class LocalEventStore : EventStoreReader, EventStoreWriter {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
     private val storage: MutableMap<String, MutableList<Event>> = mutableMapOf()
+    private val lock = ReentrantLock()
 
     override fun saveEvents(topicName: String, events: Iterable<Event>, expectedVersion: Int) {
         // check events
         val aggregateIds = events.map { it.id }.distinct()
         if (aggregateIds.size != 1) {
-            throw DDDRuntimeException("One and only one aggregate id expected while saving events [${aggregateIds.joinToString(",")}]")
+            throw DDDRuntimeException("One and only one aggregate id is expected while saving events [${aggregateIds.joinToString(",")}]")
         }
         val aggregateId = aggregateIds.first()
 
         logger.debug("Saving events for [$topicName] with Id [$aggregateId]")
-        val storedEvents = storage.getOrElse(aggregateId.toString(), { mutableListOf() })
-        val actualVersion = storedEvents.lastOrNull()?.version ?: 0
-        if (actualVersion != expectedVersion && expectedVersion != -1) {
-            throw ConcurrentModificationException("The actual version is [$actualVersion] and the expected version is [$expectedVersion]")
-        }
 
-        // set event versions
-        var version = expectedVersion
-        for (event in events) {
-            version++
-            event.version = version
-            storedEvents.add(event)
+        lock.lock()
+        try {
+            val storedEvents = storage.getOrElse(aggregateId.toString(), { mutableListOf() })
+            val actualVersion = storedEvents.lastOrNull()?.version ?: 0
+            if (actualVersion != expectedVersion && expectedVersion != -1) {
+                throw ConcurrentModificationException("The actual version is [$actualVersion] and the expected version is [$expectedVersion]")
+            }
+
+            // set event versions
+            var version = expectedVersion
+            for (event in events) {
+                version++
+                event.version = version
+                storedEvents.add(event)
+            }
+            storage.put(aggregateId.toString(), storedEvents)
+        } finally {
+            lock.unlock()
         }
-        storage.put(aggregateId.toString(), storedEvents)
     }
 
     override fun getEventsForAggregate(topicName: String, id: AggregateRootId): Iterable<Event> {
