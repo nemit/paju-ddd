e22e8234c01c27383190c81063447ac4c6ddf457
Ville Vuori <ville.vuori@nemit.fi>
2017-11-06 21:48:45 +0200

    Try exhaustive when with sealed commands

--- example/ddd-salesorder-domain/src/main/kotlin/io/paju/salesorder/command/SalesOrderCommandHandler.kt
+++ example/ddd-salesorder-domain/src/main/kotlin/io/paju/salesorder/command/SalesOrderCommandHandler.kt
@@ -1,6 +1,5 @@
 package io.paju.salesorder.command
 
-import io.paju.ddd.Command
 import io.paju.ddd.CommandHandler
 import io.paju.salesorder.domain.SalesOrder
 import io.paju.salesorder.service.DummyPaymentService
@@ -8,10 +7,21 @@ import io.paju.salesorder.service.DummyPaymentService
 class SalesOrderCommandHandler(
     private val aggregate: SalesOrder,
     private val paymentService: DummyPaymentService
-) : CommandHandler {
-    override fun handle(command: Command) {
+) : CommandHandler<SalesOrderCommand> {
+
+    override fun handle(command: SalesOrderCommand) {
         when (command) {
-            //is CreateSalesOrder -> aggregate.
+            is CreateSalesOrder ->
+                Unit // TODO
+
+            is DeliverProductsAndServices ->
+                Unit // TODO
+
+            is  DeleteSalesOrder ->
+                Unit // TODO
+
+            is ConfirmSalesOrder ->
+                Unit // TODO
 
             is AddProductToSalesOrder ->
                 aggregate.addProduct(command.product)
@@ -24,6 +34,6 @@ class SalesOrderCommandHandler(
 
             is PayDeliveredProducts ->
                 aggregate.payDeliveredProduct(paymentService, command.product, command.method)
-        }
+        }.let {} // let is required for exhaustive when
     }
 }
