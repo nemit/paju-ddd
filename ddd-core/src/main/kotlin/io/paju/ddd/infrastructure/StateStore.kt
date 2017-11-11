1c0b50b18af89f03cf402632b93b7b7640a3ffc4
Ville Vuori <ville.vuori@nemit.fi>
2017-11-11 16:19:59 +0200

    More explicit state storage

--- ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/StateStore.kt
+++ ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/StateStore.kt
@@ -2,9 +2,14 @@ package io.paju.ddd.infrastructure
 
 import io.paju.ddd.AggregateRootId
 import io.paju.ddd.State
+import io.paju.ddd.StateChangeEvent
 
-interface StateStoreWriter<in A: State> {
-    fun saveState(state: A, expectedVersion: Int)
+interface StateStoreEventWriter<in E: StateChangeEvent> {
+    fun saveState(id: AggregateRootId, events: Iterable<E>, expectedVersion: Int)
+}
+
+interface StateStoreStateWriter<in A: State> {
+    fun saveState(id: AggregateRootId, state: A, expectedVersion: Int)
 }
 
 interface StateStoreReader<out A: State> {
