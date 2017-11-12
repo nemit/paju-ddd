31efb39bb43c0d43c61a1a80e8f08bb70b2ccedf
Ville Vuori <ville.vuori@nemit.fi>
2017-11-12 21:03:18 +0200

    More explicit state store api

--- ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/StateStore.kt
+++ ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/StateStore.kt
@@ -3,16 +3,38 @@ package io.paju.ddd.infrastructure
 import io.paju.ddd.AggregateRootId
 import io.paju.ddd.State
 import io.paju.ddd.StateChangeEvent
+import io.paju.ddd.exception.DddRuntimeException
 
-interface StateStoreEventWriter<in E: StateChangeEvent> {
+interface StateStoreEventWriter {
+    fun <E: StateChangeEvent>saveState(id: AggregateRootId, events: Iterable<E>, expectedVersion: Int)
+}
+
+interface StateStoreStateWriter {
+    fun <S: State>saveState(id: AggregateRootId, state: S, expectedVersion: Int)
+}
+
+interface StateStoreReader {
+    fun <S: State>readState(id: AggregateRootId, clazz: Class<S>): S?
+
+    @Throws(DddRuntimeException::class)
+    fun <S: State>readStateOrFail(id: AggregateRootId, clazz: Class<S>): S {
+        return readState(id, clazz) ?: throw DddRuntimeException("Aggregate [$id] state not found")
+    }
+}
+
+interface StateStoreTypedEventWriter<in E: StateChangeEvent> {
     fun saveState(id: AggregateRootId, events: Iterable<E>, expectedVersion: Int)
 }
 
-interface StateStoreStateWriter<in A: State> {
-    fun saveState(id: AggregateRootId, state: A, expectedVersion: Int)
+interface StateStoreTypedStateWriter<in S: State> {
+    fun saveState(id: AggregateRootId, state: S, expectedVersion: Int)
 }
 
-interface StateStoreReader<out A: State> {
-    fun readState(id: AggregateRootId): A
-}
+interface StateStoreTypedReader<out S: State> {
+    fun readState(id: AggregateRootId): S?
 
+    @Throws(DddRuntimeException::class)
+    fun readStateOrFail(id: AggregateRootId): S {
+        return readState(id) ?: throw DddRuntimeException("Aggregate [$id] state not found")
+    }
+}
