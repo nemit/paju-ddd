3531f2d40697a6d5b195249703339499ef352bb7
KimmoEklund <kimmo.eklund@nemit.fi>
2017-11-22 12:22:48 +0200

    Support for embedded resources

--- examples/spring-rest/src/main/kotlin/io/paju/salesorder/infrastructure/ports/resources/SalesOrderResourceAssembler.kt
+++ examples/spring-rest/src/main/kotlin/io/paju/salesorder/infrastructure/ports/resources/SalesOrderResourceAssembler.kt
@@ -3,9 +3,10 @@ package io.paju.salesorder.infrastructure.ports.resources
 import io.paju.salesorder.domain.state.SalesOrderState
 import io.paju.salesorder.infrastructure.ports.controllers.SalesOrderController
 import org.springframework.hateoas.Link
-import org.springframework.hateoas.mvc.ResourceAssemblerSupport
+import org.springframework.hateoas.Resources
+import org.springframework.hateoas.core.EmbeddedWrapper
 
-class SalesOrderResourceAssembler : ResourceAssemblerSupport<SalesOrderState, SalesOrderResource>(SalesOrderController::class.java, SalesOrderResource::class.java) {
+class SalesOrderResourceAssembler : EmbeddableResourceAssemblerSupport<SalesOrderState, SalesOrderResource, SalesOrderController>(SalesOrderController::class.java, SalesOrderResource::class.java) {
 
     override fun toResource(entity: SalesOrderState): SalesOrderResource {
         val r = createResourceWithId(entity.customerId.toString(), entity)
@@ -20,6 +21,14 @@ class SalesOrderResourceAssembler : ResourceAssemblerSupport<SalesOrderState, Sa
     }
 
     override fun instantiateResource(entity: SalesOrderState): SalesOrderResource {
-        return SalesOrderResource(entity.customerId.toString(), entity.confirmed, entity.deleted, entity.products)
+        val customerAssembler = CustomerResourceAssember()
+        // in real life if we'd like to embedded customer resource we'd either
+        // a) fetch it from repository if resource is local
+        // b) fetch wit HTTP is resource is remote
+        // alternative to embedding is to add customer simply as link
+        val customer = Customer("Kimmo", "Eklund", entity.customerId.toString())
+        val embeddables = mutableListOf<EmbeddedWrapper>()
+        embeddables.add(customerAssembler.toEmbeddable(customer))
+        return SalesOrderResource(Resources<EmbeddedWrapper>(embeddables), entity.customerId.toString(), entity.confirmed, entity.deleted, entity.products)
     }
 }
