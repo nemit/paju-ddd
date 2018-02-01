7c95a0dc1d11b3ec163fcd1401f19b7e576efe87
KimmoEklund <kimmo.eklund@nemit.fi>
2018-02-01 11:37:14 +0200

    adding default implementation that returns null if exception is thrown

--- ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/Repository.kt
+++ ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/Repository.kt
@@ -3,10 +3,17 @@ package io.paju.ddd.infrastructure
 import io.paju.ddd.AggregateRoot
 import io.paju.ddd.AggregateRootId
 import io.paju.ddd.StateChangeEvent
+import io.paju.ddd.exception.DddRuntimeException
 import java.util.UUID
 
 interface Repository<E: StateChangeEvent, T : AggregateRoot<*, E>> {
     fun save(aggregate: T, version: Int)
     fun getById(id: AggregateRootId): T
-    fun getById(id: UUID): T?
+    fun getById(id: UUID): T? {
+        return try {
+            getById(AggregateRootId(id))
+        } catch (e: DddRuntimeException) {
+            null
+        }
+    }
 }
