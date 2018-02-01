0fdd18f1e01a5851b791420d438a33b5a9e51b26
KimmoEklund <kimmo.eklund@nemit.fi>
2018-02-01 11:11:07 +0200

    Added nullable version of getById that uses UUID as param type

--- ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/Repository.kt
+++ ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/Repository.kt
@@ -3,8 +3,10 @@ package io.paju.ddd.infrastructure
 import io.paju.ddd.AggregateRoot
 import io.paju.ddd.AggregateRootId
 import io.paju.ddd.StateChangeEvent
+import java.util.UUID
 
 interface Repository<E: StateChangeEvent, T : AggregateRoot<*, E>> {
     fun save(aggregate: T, version: Int)
     fun getById(id: AggregateRootId): T
+    fun getById(id: UUID): T?
 }
