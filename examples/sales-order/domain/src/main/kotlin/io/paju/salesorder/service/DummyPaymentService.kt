a8c822da52488a7a4c9a730626c827039eed49d8
Ville Vuori <ville.vuori@nemit.fi>
2018-08-23 20:23:33 +0300

    Remove outdated example project

--- examples/sales-order/domain/src/main/kotlin/io/paju/salesorder/service/DummyPaymentService.kt
+++
@@ -1,9 +0,0 @@
-package io.paju.salesorder.service
-
-import io.paju.ddd.EntityId
-import io.paju.salesorder.domain.PaymentMethod
-import io.paju.salesorder.domain.Product
-
-interface DummyPaymentService {
-    fun handleProductPayment(product: Product, customerId: EntityId, paymentMethod: PaymentMethod)
-}
--- examples/sales-order/spring-api/src/main/kotlin/io/paju/salesorder/infrastructure/ports/resources/CommandResources.kt
+++
@@ -1,7 +0,0 @@
-package io.paju.salesorder.infrastructure.ports.resources
-
-import io.paju.ddd.EntityId
-import io.paju.salesorder.domain.PaymentMethod
-
-data class ProductPayment(val productId: EntityId, val paymentMethod: PaymentMethod)
-data class ProductDelivery(val productId: EntityId)
