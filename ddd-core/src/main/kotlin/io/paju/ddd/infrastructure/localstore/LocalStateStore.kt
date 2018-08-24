29ed8d4a50bedaeb7e7f9fd46e91a2447fb3dee0
Ville Vuori <ville.vuori@nemit.fi>
2018-08-24 10:37:15 +0300

    Remove state store interfaces because repository is enough

--- ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/localstore/LocalStateStore.kt
+++ ddd-core/src/main/kotlin/io/paju/ddd/infrastructure/localstore/LocalStateStore.kt
@@ -1,45 +1,35 @@
 package io.paju.ddd.infrastructure.localstore
 
 import io.paju.ddd.State
-import io.paju.ddd.infrastructure.StateStoreReader
-import io.paju.ddd.infrastructure.StateStoreStateWriter
 import io.paju.logger
 import java.util.ConcurrentModificationException
 import java.util.UUID
 import java.util.concurrent.locks.ReentrantLock
 
-class LocalStateStore : StateStoreStateWriter, StateStoreReader {
-
+class LocalStateStore<S: State> {
     private val logger = logger()
-    private val storage: MutableMap<String, State> = mutableMapOf()
+    private val storage: MutableMap<UUID, S> = mutableMapOf()
     private val lock = ReentrantLock()
 
-    private fun <S: State>getKey(id: UUID, clazz: Class<S>): String =
-        "${clazz.simpleName}:$id"
-
-    override fun <S: State>saveState(
+    fun saveState(
         id: UUID,
         state: S,
         expectedVersion: Int)
     {
         logger.debug("Saving state for Id [$id]")
 
-        val key = getKey(id, state.javaClass)
         lock.lock()
         try {
-            val storedState = storage[key]
+            val storedState = storage[id]
             val actualVersion = storedState?.version() ?: 1
             if (actualVersion != expectedVersion && expectedVersion != -1) {
                 throw ConcurrentModificationException("The actual version is [$actualVersion] and the expected version is [$expectedVersion]")
             }
-            storage.put(key, state)
+            storage.put(id, state)
         } finally {
             lock.unlock()
         }
     }
 
-    override fun <S: State>readState(id: UUID, clazz: Class<S>): S? {
-        val state = storage[getKey(id, clazz)]
-        return if (state != null) clazz.cast(state) else null
-    }
+    fun readState(id: UUID): S? = storage[id]
 }
